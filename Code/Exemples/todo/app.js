2b2a98eb248f85d9ea44dda5a9bb78f8a808f663
Maxime Ricco <maxime.ricco.60@gmail.com>
2021-02-09 16:42:55 +0100

    ajout js

---
+++ Code/Exemples/todo/app.js
@@ -0,0 +1,5 @@
+//SELECTEURS
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
